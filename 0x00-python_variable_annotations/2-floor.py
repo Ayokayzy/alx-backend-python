e0bdfc967a6e230e8f1abb287754f677241830d6
Ayokayzy <theayokayzy1@gmail.com>
2024-10-16 05:50:59 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+2-floor.py
+"""
+import math
+
+
+def floor(n:float) -> int:
+    """
+    takes a float n as argument and
+    returns the floor of the float
+
+    Parameters:
+    n (float): a decimal number
+    """
+
+    whole_num: int = math.floor(num)

dea34cd1265aa97f54576d1c8884fcc8d1e84780
Ayokayzy <theayokayzy1@gmail.com>
2024-10-16 05:41:17 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/vin/env python3
+"""
+1-concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    takes a string str1 and a string str2 as
+    arguments and returns a concatenated string
+    """
+    full_str: str = str1 + str2
+    return full_str
